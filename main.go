13b7d51966e414a4d06ff8d3a751fcbbdcf9f2bf
SEANGLAY <liseanglay@gmail.com>
2023-02-25 16:18:32 +0700

    initiate

---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+	os "os"
+	"time"
+)
+
+var (
+	router = gin.Default()
+)
+
+func main() {
+	router.POST("login", Login)
+	log.Fatal(router.Run(":8080"))
+}
+
+type User struct {
+	ID       uint64 `json:"id"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Phone    string `json:"phone"`
+}
+
+var user = User{
+	ID: 1, Username: "Seanglay", Password: "123456", Phone: "012345678",
+}
+
+func Login(c *gin.Context) {
+	var u User
+	if err := c.ShouldBindJSON(&u); err != nil {
+		c.JSON(http.StatusUnprocessableEntity, "Invalid json provided")
+		return
+	}
+
+	if user.Username != u.Username || user.Password != u.Password {
+		c.JSON(http.StatusUnauthorized, "Please provide valid login details")
+		return
+	}
+	token, err := CreateToken(user.ID)
+	if err != nil {
+		c.JSON(http.StatusUnprocessableEntity, err.Error())
+		return
+	}
+	c.JSON(http.StatusOK, token)
+}
+
+func CreateToken(userId uint64) (string, error) {
+	var err error
+	os.Setenv("ACCESS_TOKEN", "Seanglay")
+	atClaims := jwt.MapClaims{}
+	atClaims["authorized"] = true
+	atClaims["user_id"] = userId
+	atClaims["exp"] = time.Now().Add(time.Minute * 15).Unix()
+	at := jwt.NewWithClaims(jwt.SigningMethodHS256, atClaims)
+	token, err := at.SignedString([]byte(os.Getenv("ACCESS_SECRET")))
+	if err != nil {
+		return "", err
+	}
+	return token, nil
+}
